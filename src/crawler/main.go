065b9f07c0a5efabb337fca3b4a0482dca7e98ba
yangyonghui <yangyonghui@meituan.com>
2018-06-29 10:05:49 +0800

    add xmlpath

--- src/crawler/main.go
+++ src/crawler/main.go
@@ -1,1 +1,36 @@
-package crawler
+package main
+
+import (
+	"net/http"
+	"log"
+	"gopkg.in/xmlpath.v2"
+	"fmt"
+)
+
+func main() {
+	res, err := http.Get("https://wwww.baidu.com/")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Print(err)
+	}
+
+	defer res.Body.Close()
+	//body, _ := ioutil.ReadAll(res.Body)
+	//fmt.Println(string(body))
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		fmt.Print(err)
+	}
+	fmt.Println("here")
+	root, err := xmlpath.ParseHTML(res.Body)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	fmt.Println("here")
+	path, err := xmlpath.Compile("//title")
+
+	if value, ok := path.String(root); ok {
+		fmt.Println("Found:", value)
+	}
+}
